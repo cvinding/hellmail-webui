3d067c4d84981ffd147dcc79ffb710c2077ec770
tobias970 <tobias-97@outlook.dk>
2019-04-02 08:56:08 +0200

    templates

--- public_html/index.php
+++ public_html/index.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html lang="dk">
-<head>
-    <meta charset="UTF-8">
-    <title>Hjem</title>
-    <link rel="stylesheet" href="libs/bootstrap-4.3.1-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="libs/font-awesome/css/all.css">
-    <link rel="stylesheet" href="/css/style.css">
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hjem</title>
+        <link rel="stylesheet" href="libs/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="libs/font-awesome/css/all.css">
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
+    <body>
 
+        <?php require_once "../templates/banner.html"; ?>
 
-</body>
+        <?php require_once "../templates/navbar.html"; ?>
+
+        <?php require_once "../templates/footer.html"; ?>
+
+    </body>
 </html>
 <script src="libs/jquery/jquery-3.3.1.min.js"></script>
 <script src="libs/popper/popper.min.js"></script>
