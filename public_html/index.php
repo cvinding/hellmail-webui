7253a377b0dfdf7599dd99323e47ef1df4a435f9
tobias970 <tobias-97@outlook.dk>
2019-04-02 08:48:52 +0200

    Libs

---
+++ public_html/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="dk">
+<head>
+    <meta charset="UTF-8">
+    <title>Hjem</title>
+    <link rel="stylesheet" href="libs/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="libs/font-awesome/css/all.css">
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+
+
+</body>
+</html>
+<script src="libs/jquery/jquery-3.3.1.min.js"></script>
+<script src="libs/popper/popper.min.js"></script>
+<script src="libs/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
