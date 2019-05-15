d116f5daf2d4da98c18b28de9033fcb1dc2907ec
tobias970 <tobias-97@outlook.dk>
2019-05-15 09:33:38 +0200

    alt virker

--- public_html/php/getMails.php
+++ public_html/php/getMails.php
@@ -1,17 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tubbe
- * Date: 14-05-2019
- * Time: 08:40
- */
+error_reporting(E_ALL);
 
-    $mails = "To: tubbe@hellmail.dk
+
+$mails = "To: tubbe@hellmail.dk
 From: kent@hellmail.dk
 Subject: Hej!
 
-Jeg er bøsse :)
+\"Jeg er bøsse :)\"
 .
+
 To: tubbe@hellmail.dk
 From: kent@hellmail.dk
 Cc: christian@hellmail.dk
@@ -20,6 +17,7 @@ Subject: Rettelse!
 \\\"Jeg er IKKE bøsse! Sidste mail var en fejl!!!
 hilsen kent\\\"
 .
+
 To: kent@hellmail.dk
 From: tubbe@hellmail.dk
 Cc: christian@hellmail.dk,chefen@hellmail.dk
@@ -29,7 +27,7 @@ Hold op med at spamme spasser
 hilsen tubbe
 .";
 
-getMailData($mails);
+//getMailData($mails);
 
     $separator = "\r\n";
     $line = strtok($mails, $separator);
@@ -48,72 +46,137 @@ getMailData($mails);
     {
         $strlength = strlen($mails);
 
-        foreach (strpos($mails, 'To:') as $mails){
-            
+        $start = strpos($mails, 'To:');
+        $slut = strripos($mails, '\\"');
+
+        echo ("SLUTTER : $slut");
+        if ($start === 0){
+
+
+
+            if ($start === 0 and strpos($mails, 'To:') !== false) {
+                $findtilstart = strpos($mails, 'To:')+3;
+
+                $findtilslut = strpos($mails, "\n")-3;
+                echo("TO-nr : $findtilstart $findtilslut");
+                $toperson = substr($mails, $findtilstart, $findtilslut);
+                //echo $toperson;
+
+                $mails = substr($mails, $findtilslut+4, $strlength);
+                //$slut = $findtilslut;
+            }
+
+            $fromslut = strpos($mails, 'From:');
+
+            if (strpos($mails, 'From:') !== false and $fromslut < $slut) {
+                $findfromstart = strpos($mails, 'From:')+5;
+
+                $findfromslut = strpos($mails, "\n")-5;
+                echo("FROM-nr : $findfromstart $findfromslut");
+                $fromperson = substr($mails, $findfromstart, $findfromslut);
+                //echo $fromperson;
+
+                $mails = substr($mails, $findfromslut+6, $strlength);
+                //$slut = $findfromslut;
+            }
+           if (strpos($mails, 'Cc:') !== false) {
+               $findccstart = strpos($mails, 'Cc:')+3;
+
+               $findccslut = strpos($mails, "\n")-3;
+               echo("Cc : $findccstart $findccslut");
+               $subjectmail = substr($mails, $findccstart, $findccslut);
+               //echo $fromperson;
+
+               //$mails = substr($mails, $findfromslut, $strlength);
+               //$slut = $findfromslut;
+            }
+    /*        if (strpos($mails, 'Bcc:') !== false) {
+                echo("FRA");
+                $fromperson = $mails;
+            }
+            if (strpos($mails, 'Subject:') !== false) {
+                $findemnestart = strpos($mails, 'Subject:');
+
+                $findemneslut = strpos($mails, "\n");
+                echo("EMNE : $findemnestart $findemneslut");
+            }
+            if (strpos($mails, '\\"') !==false) {
+                echo("Besked");
+            }
+
+            if (strpos($mails, '.') !==false) {
+                echo("SLUT");
+            }*/
+
+            echo $toperson;
+            echo $fromperson;
+            echo $subjectmail;
         }
-
-        if (strpos($mails, 'To:') !== false) {
-            $findtilstart = strpos($mails, 'To:')+3;
-
-            $findtilslut = strpos($mails, "\n")-3;
-            //echo("TO-nr : $findtilstart $findtilslut");
-            $toperson = substr($mails, $findtilstart, $findtilslut);
-            //echo $toperson;
-
-            $mails = substr($mails, $findtilslut+4, $strlength);
-            //$slut = $findtilslut;
-        }
-
-        if (strpos($mails, 'From:') !== false) {
-            $findfromstart = strpos($mails, 'From:')+5;
-
-            $findfromslut = strpos($mails, "\n")-5;
-            //echo("FROM-nr : $findfromstart $findfromslut");
-            $fromperson = substr($mails, $findfromstart, $findfromslut);
-            //echo $fromperson;
-
-            //$mails = substr($mails, $findfromslut, $strlength);
-            //$slut = $findfromslut;
-        }
-       if (strpos($mails, 'Cc:') !== false) {
-           $findccstart = strpos($mails, 'Cc:')+3;
-
-           $findccslut = strpos($mails, "\n")-3;
-           echo("Subject-nr : $findccstart $findccslut");
-           $subjectmail = substr($mails, $findccstart, $findccslut);
-           //echo $fromperson;
-
-           //$mails = substr($mails, $findfromslut, $strlength);
-           //$slut = $findfromslut;
-        }
-/*        if (strpos($mails, 'Bcc:') !== false) {
-            echo("FRA");
-            $fromperson = $mails;
-        }
-        if (strpos($mails, 'Subject:') !== false) {
-            $findemnestart = strpos($mails, 'Subject:');
-
-            $findemneslut = strpos($mails, "\n");
-            echo("EMNE : $findemnestart $findemneslut");
-        }
-        if (strpos($mails, '\\"') !==false) {
-            echo("Besked");
-        }
-
-        if (strpos($mails, '.') !==false) {
-            echo("SLUT");
-        }*/
-
-        //echo $toperson;
-        //echo $fromperson;
-        echo $subjectmail;
     }
 
         //echo ("Linjenr : $linjenr $mails");
 
-        $line = strtok( $separator );
+        //$line = strtok( $separator );
     //}
 
+
+separateMails($mails);
+
+    function separateMails(string $mailData) : array {
+
+        $rTags = ["To","From","Subject"];
+        $oTags = ["Cc","Bcc"];
+
+        $startIndex = 0;
+        $endIndex = 0;
+
+        while(true) {
+
+            if($endIndex !== 0) {
+                $startIndex = $endIndex++;
+            }
+
+            $endIndex = strpos(substr($mailData, $startIndex), "\"".PHP_EOL."." . PHP_EOL);
+
+            $mail = substr($mailData, $startIndex, $endIndex);
+
+            //echo $mail.PHP_EOL;
+
+            foreach($rTags as $tag) {
+
+                $index = strpos($mail, PHP_EOL."".$tag.":");
+
+                if($index === false) {
+                    $index = strpos($mail, $tag.":");
+
+                    if($index !== 0) {
+                        echo "CONTINUED";
+                        continue;
+                    }
+                }
+
+                //echo $tag . " : " . strlen($tag) . "<br>";
+
+                echo substr($mail,$index, $index+7)."<br>";
+
+                $start = $index + strlen($tag) + 2;
+
+                $tagData = substr($mail, $start, strpos(substr($mail, $start), PHP_EOL));
+
+
+                //echo $tagData."<br>";
+
+                //echo substr($mail, strlen($tag) + 2).PHP_EOL.PHP_EOL.PHP_EOL;
+
+                //echo( $tagData).PHP_EOL;
+
+            }
+
+            break;
+        }
+
+        return [0 => ["to" => "tubbe@hellmail.dk"]];
+    }
 
 
 ?>
